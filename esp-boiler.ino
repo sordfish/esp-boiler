e3ee6358958a51dc7942b2232eb47c28d3b89f7a
Arran Short <sordfish2050@gmail.com>
2017-12-05 14:50:50 +0000

    basic working code

--- esp-boiler.ino
+++ esp-boiler.ino
@@ -1,76 +1,116 @@
+#include <IRsend.h>
+#include <IRremoteESP8266.h>
+
 #include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library (you most likely already have this in your sketch)
 
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
-#include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
-
-#include <IRremote.h>
+#include <WiFiManager.h>     
  
-IRsend irsend;
+IRsend irsend(4);
  
 // just added my own array for the raw signal
-unsigned int digitalOne[4] = {0,820,370,0};
-unsigned int radio_preamble_a[3] = {4800,820,0};
-unsigned int radio_preamble_b[3] = {4800,0,0};
-unsigned int digitalZero[4] = {0,200,370,0};
+uint16_t digitalOne[4] = {0,900,325,0};
+uint16_t radio_preamble_a[3] = {4500,900,0};
+uint16_t radio_preamble_b[3] = {4500,0,0};
+uint16_t digitalZero[4] = {0,300,305,0};
+
 //off
-unsigned int heating_code_first[33];
-const unsigned int heating_code_second[33] =
-{1,0,0,0,1,0,0,0,1,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1};
-String inputString = "";         // a string to hold incoming data
-boolean stringComplete = false;  // whether the string is completevoid 
+const unsigned int boiler_on_code[33]      = {1,0,0,0,1,0,0,0,1,0,1,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1};
+const unsigned int boiler_on_inv_code[33]  = {1,1,1,1,0,1,1,1,0,1,0,1,1,0,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,0};
+
+const unsigned int boiler_off_code[33]     = {1,0,0,0,1,0,0,0,1,1,1,0,0,1,0,1,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0};
+const unsigned int boiler_off_inv_code[33] = {1,1,1,1,0,1,1,1,0,0,0,1,1,0,1,0,1,1,1,1,1,1,1,1,0,1,1,1,1,1,1,1,1};
+
+ESP8266WebServer server(80);
+
+void boilerOn(){
+
+  unsigned int pulse_count=0;
+
+  for (pulse_count=0;pulse_count < 4;pulse_count++) {
+    int bit_count =0;
+    irsend.sendRaw(radio_preamble_b,3,40);
+    for (bit_count=0;bit_count < 33;bit_count++) {
+      if (boiler_on_code[bit_count] == 1) {
+        irsend.sendRaw(digitalOne,4,40);
+      } else {
+        irsend.sendRaw(digitalZero,4,40);
+      }
+    }
+
+    delay(28);//Big delay between two pulses.
+    irsend.sendRaw(radio_preamble_a,3,40);
+    for (bit_count=0;bit_count < 33;bit_count++) {
+      if (boiler_on_inv_code[bit_count] == 1) {
+        irsend.sendRaw(digitalOne,4,40);
+      } else {
+        irsend.sendRaw(digitalZero,4,40);
+      }
+    }
+    delay(15);
+  }
+}
+
+void boilerOff(){
+  unsigned int pulse_count=0;
+
+  for (pulse_count=0;pulse_count < 4;pulse_count++) {
+    int bit_count =0;
+    irsend.sendRaw(radio_preamble_b,3,40);
+    for (bit_count=0;bit_count < 33;bit_count++) {
+      if (boiler_off_code[bit_count] == 1) {
+        irsend.sendRaw(digitalOne,4,40);
+      } else {
+        irsend.sendRaw(digitalZero,4,40);
+      }
+    }
+
+    delay(28);//Big delay between two pulses.
+    irsend.sendRaw(radio_preamble_a,3,40);
+    for (bit_count=0;bit_count < 33;bit_count++) {
+      if (boiler_off_inv_code[bit_count] == 1) {
+        irsend.sendRaw(digitalOne,4,40);
+      } else {
+        irsend.sendRaw(digitalZero,4,40);
+      }
+    }
+    delay(15);
+  }  
+}
 
 
 void setup() {
   WiFiManager wifiManager;
   wifiManager.autoConnect("ESP-BOILER");
+
+  //ESP8266WebServer server(80);
+  
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    //Serial.print(".");
+  }
+
+  irsend.begin();
+  
+  server.on("/", [](){
+    server.send(200, "text/plain", "root");
+  });
+
+  server.on("/on", [](){
+    boilerOn();
+    server.send(200, "text/plain", "on signal sent");
+  });
+  
+  server.on("/off", [](){
+    boilerOff();
+    server.send(200, "text/plain", "off signal sent");
+  });
+
+  server.begin();
+  
 }
 
 void loop() {
-   unsigned int input_num = inputString.toInt();
- inputString = "";
- stringComplete = false;
- unsigned int i=0;
- for (i=0;i < 33;i++) {
-    heating_code_first[i] = heating_code_second[i];
- }
- if (input_num == 1) {//heating off
- if (heating_code_first[32] == 0) {
- heating_code_first[32] =1;
- } else {
- heating_code_first[32] =0;
- } 
-  if (heating_code_first[9] == 0) {
-  heating_code_first[9] =1;
-  } else {
-  heating_code_first[9] =0;
-  }
- }
- unsigned int pulse_count=0;
- for (pulse_count=0;pulse_count < 4;pulse_count++) {
-  int bit_count =0;
-  irsend.sendRaw(radio_preamble_b,3,40);
-  for (bit_count=0;bit_count < 33;bit_count++) {
-   if (heating_code_first[bit_count] == 1) {
-   irsend.sendRaw(digitalOne,4,40);
-   } else {
-   irsend.sendRaw(digitalZero,4,40);
-  }
- }
- 
-  delay(28);//Big delay between two pulses.
-  irsend.sendRaw(radio_preamble_a,3,40);
-  for (bit_count=0;bit_count < 33;bit_count++) {
-   if (heating_code_first[bit_count] == 0) {
-   irsend.sendRaw(digitalOne,4,40);
-   } else {
-   irsend.sendRaw(digitalZero,4,40);
-   }
-  }
- delay(15);
- }
- }
- 
-//delay(2000);
-
+  server.handleClient();
 }
